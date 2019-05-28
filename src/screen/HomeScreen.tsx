75715cd2587020d8de5be6d63e384092b39f4726
杨俊宁 <1003719811@qq.com>
2019-05-28 11:39:54 +0800

    react-native-template不支持有图片

--- src/screen/HomeScreen.tsx
+++ src/screen/HomeScreen.tsx
@@ -1,12 +1,41 @@
 import React, { Component } from 'react'
-import { View, Image } from 'react-native'
+import { Platform, StyleSheet, Text, View } from 'react-native'
 
-export default class HomeScreen extends Component {
-  render() {
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+})
+
+interface Props {}
+class HomeScreen extends Component<Props> {
+  public render() {
     return (
-      <View>
-        <Image source={require('~/images/home.png')} />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  instructions: {
+    color: '#333333',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  welcome: {
+    fontSize: 20,
+    margin: 10,
+    textAlign: 'center',
+  },
+})
+
+export default HomeScreen
