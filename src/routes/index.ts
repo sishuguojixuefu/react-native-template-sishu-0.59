cdcb6f64211eb8eafa8d7175211d169bcb315b8f
Eva <1003719811@qq.com>
2019-06-09 11:14:38 +0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
 import { createAppContainer, createStackNavigator } from 'react-navigation'
-import HomeScreen from '~/screens/HomeScreen'
+import Home from '~/screens/HomeScreen'
 
 // 把 Routes 放在 enum 以此来避免引用时的拼写错误。
 export enum ROUTES {
   MainStack = 'MainStack',
-  Home = 'Home',
+  HomeScreen = 'HomeScreen',
 }
 
 // The stack for the main navigation
@@ -12,8 +12,8 @@ export enum ROUTES {
 // createStackNavigator是一个函数，它接受一个路由配置对象和一个可选配置对象并返回一个 React 组件。
 const MainStack = createStackNavigator(
   {
-    [ROUTES.Home]: {
-      screen: HomeScreen, // 配置中唯一必须的属性是screen（此项设置一个组件）
+    [ROUTES.HomeScreen]: {
+      screen: Home, // 配置中唯一必须的属性是screen（此项设置一个组件）
       navigationOptions: {
         title: 'Home', // 可用作 headerTitle 的回退的字符串。 此外, 将用作 tabBarLabel 的回退 (如果嵌套在 TabNavigator 中) 或 drawerLabel (如果嵌套在DrawerNavigator)
         headerTitle: '首页', // 支持自定义组件，默认是一个 Text
@@ -21,7 +21,7 @@ const MainStack = createStackNavigator(
     },
   },
   {
-    initialRouteName: ROUTES.Home, // 指定堆栈中的初始路由
+    initialRouteName: ROUTES.HomeScreen, // 指定堆栈中的初始路由
   }
 )
 
