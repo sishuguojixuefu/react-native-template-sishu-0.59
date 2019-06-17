fc36f64a1dad5bf1fde2ecf98088a5d705fdb687
Eva <1003719811@qq.com>
2019-06-17 13:57:22 +0800

    Update request.ts

--- src/request/request.ts
+++ src/request/request.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 // 中文文档: http://t.cn/ROfXFuj
 // 创建实例
 const request = axios.create({
-  baseURL: '', // 配置baseURL
+  baseURL: '', // TODO: 配置baseURL
   timeout: 10000,
   headers: { platform: 'app' },
 })
@@ -10,11 +10,11 @@ const request = axios.create({
 // 添加请求拦截器
 request.interceptors.request.use(
   config => {
-    // 在发送请求之前做些什么
+    // TODO: 在发送请求之前做些什么
     return config
   },
   error => {
-    // 对请求错误做些什么
+    // TODO: 对请求错误做些什么
     return Promise.reject(error)
   }
 )
@@ -22,11 +22,11 @@ request.interceptors.request.use(
 // 添加响应拦截器
 request.interceptors.response.use(
   response => {
-    // 对响应数据做点什么
+    // TODO: 对响应数据做点什么
     return response
   },
   error => {
-    // 对响应错误做点什么
+    // TODO: 对响应错误做点什么
     return Promise.reject(error)
   }
 )
