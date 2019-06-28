53ad18147dea13f1c84a307c96a1c6c5b9d24372
杨俊宁 <1003719811@qq.com>
2019-06-28 13:35:59 +0800

    放弃把mobx的方法挂载到全局

--- global.ts
+++ global.ts
@@ -1,6 +1,4 @@
 import { Dimensions, Platform, StyleSheet } from 'react-native'
-import { observable, action, runInAction, computed, toJS } from 'mobx'
-import { observer, inject } from 'mobx-react'
 import { ROUTES } from './src/routes'
 import getDeviceInfo from './src/utils/getDeviceInfo'
 
@@ -11,12 +9,5 @@ global.android = Platform.OS === 'android'
 global.windowHeight = height
 global.windowWidth = width
 global.hairlineWidth = StyleSheet.hairlineWidth
-global.observable = observable
-global.action = action
-global.runInAction = runInAction
-global.computed = computed
-global.toJS = toJS
-global.observer = observer
-global.inject = inject
 global.DeviceInfo = getDeviceInfo()
 global.ROUTES = ROUTES
--- types/global.d.ts
+++ types/global.d.ts
@@ -19,31 +19,6 @@ declare namespace global {
    * 这一常量定义了当前平台上的最细的宽度。可以用作边框或是两个元素间的分隔线
    */
   let hairlineWidth: number
-  /**
-   * Observable 值可以是JS基本数据类型、引用类型、普通对象、类实例、数组和映射
-   */
-  let observable: any
-  /**
-   * 应该永远只对修改状态的函数使用动作。
-   * 只执行查找，过滤器等函数不应该被标记为动作，以允许 MobX 跟踪它们的调用。
-   */
-  let action: any
-  /**
-   * 将 “最终的” 修改放入一个异步动作中
-   */
-  let runInAction: any
-  /**
-   * 计算值(computed values)是可以根据现有的状态或其它计算值衍生出的值
-   */
-  let computed: any
-  /**
-   * 递归地将一个(observable)对象转换为 javascript 结构
-   */
-  let toJS: any
-  /**
-   * observer 组件越多，渲染效率越高
-   */
-  let observer: any
   let __DEV__: boolean
   /**
    * 利用 react-native-device-info 获取的设备信息
@@ -63,13 +38,6 @@ declare const android: boolean
 declare const windowHeight: number
 declare const windowWidth: number
 declare const hairlineWidth: number
-declare const observable: any
-declare const action: any
-declare const runInAction: any
-declare const computed: any
-declare const toJS: any
-declare const observer: any
 declare const __DEV__: boolean
 declare const DeviceInfo: any
-declare const inject: any
 declare const ROUTES: any
