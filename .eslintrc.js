08121f29f06c20d1b42aba4812e346abd21f80cd
杨俊宁 <1003719811@qq.com>
2019-07-13 11:02:20 +0800

    更新eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -103,6 +103,7 @@ module.exports = {
     'react-native/no-color-literals': 0,
     'react-native/no-raw-text': 0,
     'react-native/no-inline-styles': 0,
+    'react-native/split-platform-components': 0,
     '@typescript-eslint/explicit-member-accessibility': [2, { accessibility: 'no-public' }],
     '@typescript-eslint/no-empty-interface': 1,
     '@typescript-eslint/explicit-function-return-type': [
