e666b49a42947526e1289a47ef46000b7928b186
杨俊宁 <1003719811@qq.com>
2019-08-21 16:52:38 +0800

    删除 AuthLogdingScreen

--- app/routes/AppContainer.tsx
+++ app/routes/AppContainer.tsx
@@ -1,12 +1,10 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
 import Routes from '~/enum/Routes'
-import AuthLoadingScreen from '~/routes/AuthLoadingScreen'
 import MainStack from '~/routes/MainStack'
 
 export default createAppContainer(
   createSwitchNavigator(
     {
-      [Routes.AuthLoadingScreen]: AuthLoadingScreen,
       [Routes.MainStack]: MainStack,
     },
     {
