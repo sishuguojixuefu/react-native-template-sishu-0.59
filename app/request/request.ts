8d47d305073d1796922f6993b9be62a3b060537b
杨俊宁 <1003719811@qq.com>
2019-08-21 14:41:41 +0800

    过滤空字段

--- app/request/request.ts
+++ app/request/request.ts
@@ -1,4 +1,19 @@
 import axios from 'axios'
+import qs from 'qs'
+
+const filterParam = obj => {
+  for (const key in obj) {
+    if (typeof obj[key] === 'object') {
+      filterParam(obj[key]) // 递归遍历
+    }
+
+    if ((Array.isArray(obj[key]) && obj[key].length === 0) || !obj[key]) {
+      delete obj[key]
+    }
+  }
+  return obj
+}
+
 // 中文文档: http://t.cn/ROfXFuj
 // 创建实例
 const request = axios.create({
@@ -10,7 +25,13 @@ const request = axios.create({
 // 添加请求拦截器
 request.interceptors.request.use(
   config => {
-    // TODO: 在发送请求之前做些什么
+    // 过滤空字段
+    filterParam(config.headers)
+    config.data && filterParam(config.data)
+    config.params && filterParam(config.data)
+    if (config.method === 'get') {
+      config.paramsSerializer = params => qs.stringify(params, { arrayFormat: 'repeat' })
+    }
     return config
   },
   error => {
@@ -22,8 +43,9 @@ request.interceptors.request.use(
 // 添加响应拦截器
 request.interceptors.response.use(
   response => {
-    // TODO: 对响应数据做点什么
-    return response
+    console.info('响应数据：', response)
+    const { data } = response
+    return data
   },
   error => {
     // TODO: 对响应错误做点什么
